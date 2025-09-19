d94a0d286daae29e6add5aa5491f1955c9e2a70b
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-09-19 13:11:19 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14492 KB -BaekjoonHub

--- Java/백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ Java/백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	static int N, M;
-	static int map[][];
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static boolean[][] visit;
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
 	
-	
-	public static void main(String args[]) throws Exception {
+	static int[][] visited;
+	static int[][] map;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		visit = new boolean[N][M];
-		Queue<int[]>q = new LinkedList<>();
 		
-		// 미로를 입력받아야 함.
 		map = new int[N][M];
-		for(int i=0; i<N ; i++) {
-			String line = br.readLine();
-			for(int j=0; j<M; j++) {
-				map[i][j] = line.charAt(j) - '0';
+		visited = new int[N][M];
+		
+		for(int i=0; i<N; i++) {
+			String tmp = br.readLine();
+			for(int j=0; j<M ; j++) {
+				map[i][j] = tmp.charAt(j) - '0';
+				// 문자 -> 숫자 변환
 			}
 		}
 		
-		q.add(new int[] {0,0});
-		visit[0][0] = true;
-		
-		while(!q.isEmpty()) {
-		int[] cur = q.poll();
-		int x = cur[0];
-		int y = cur[1];
-		
-		for(int d=0; d<4 ; d++) {
-			int nx = x+ dx[d];
-			int ny = y+ dy[d];
-			
-			if(nx >= 0 && ny >= 0 && nx < N && ny < M) {
-				if(!visit[nx][ny] && map[nx][ny]==1) {
-					visit[nx][ny] = true;
-					map[nx][ny] = map[x][y] + 1;
-					q.add(new int[] {nx,ny});
-				}
-			}
-		}
-		}
-		
-		System.out.println(map[N-1][M-1]);
 		
 		
-		
-		
+		bfs(0,0);
+	
+		System.out.println(visited[N-1][M-1]);
+		//System.out.println(cnt);
 		
 	}
-	
+
+
+	private static void bfs(int sy, int sx) {
+		Queue<int[]> q = new ArrayDeque<>();
+		visited[sy][sx] = 1;
+		q.offer(new int[] {sy,sx});
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int y= cur[0]; int x=cur[1];
+			for(int d=0; d<4 ; d++) {
+				int ny = y + dy[d];
+				int nx = x + dx[d];
+				
+				if(ny <0 || nx<0 || ny >=N || nx >= M) continue;
+				
+				if(visited[ny][nx] == 0 && map[ny][nx] == 1) {
+					visited[ny][nx] = visited[y][x] + 1;
+					q.offer(new int[] {ny,nx});
+				}
+			}
+			
+		}
+		
+		
+	}// bfs
+
 }
