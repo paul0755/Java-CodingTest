b47743f4c8d506aaa6b8bbc69924357d6b61994d
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-10-08 17:05:33 +0900

    [Gold V] Title: Contact, Time: 208 ms, Memory: 17384 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int T;
+    static String pt = "(100+1+|01)+";
+
+    public static void main(String args[]) throws Exception, IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++){
+            String tmp = br.readLine();
+            if(tmp.matches(pt)){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+        
+    }
+}
