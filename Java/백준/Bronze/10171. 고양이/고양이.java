d8894e9f5ff4865b7904b2d9f6993e994b413188
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-10-15 15:54:23 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13916 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String args[]) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("\\    /\\").append("\n");
+		sb.append(" )  ( ')").append("\n");
+		sb.append("(  /  )").append("\n");
+		sb.append(" \\(__)|");
+		
+		System.out.println(sb);
+		
+	}
+}
