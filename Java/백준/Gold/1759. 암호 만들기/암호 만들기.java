8ffea5ff6cebda8803faddf1821a21617662ad80
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-10-17 14:27:01 +0900

    [Gold V] Title: 암호 만들기, Time: 128 ms, Memory: 15372 KB -BaekjoonHub

---
+++ Java/백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static int L, C;
+    static char[] alpha;
+    static String vowels = "aeiou";
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        alpha = new char[C];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<C; i++){
+            alpha[i] = st.nextToken().charAt(0);
+        }
+
+        Arrays.sort(alpha);
+
+        // for(int i=0; i<C; i++){
+        //     System.out.print(alpha[i] + " ");
+        // }
+       
+        dfs(0,0, sb);
+    }
+    private static void dfs(int start, int depth, StringBuilder sb) {
+        
+        if(depth == L){
+            if(check(sb.toString())){
+                System.out.println(sb.toString());
+            }
+
+            return;
+        }
+
+        for(int i=start; i<C; i++){
+            sb.append(alpha[i]);
+            dfs(i+1, depth+1, sb);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+    private static boolean check(String word) {
+
+        int vowelCnt = 0;
+        int consonantCnt = 0;
+        // 모음 체크
+        for(char c : word.toCharArray()){
+            if(vowels.indexOf(c) != -1) vowelCnt++;
+            else consonantCnt++;
+        }
+
+        return (vowelCnt>= 1 && consonantCnt >= 2);
+    }
+}
