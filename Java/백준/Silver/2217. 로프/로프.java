bc61b6b031055c2f94015d2ce31b5dda8fa91e8d
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-11-11 16:29:27 +0900

    [Silver IV] Title: 로프, Time: 300 ms, Memory: 26188 KB -BaekjoonHub

---
+++ Java/백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N;
+    static int[] rope;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        rope = new int[N];
+
+        for(int i=0; i<N; i++){
+            rope[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(rope);
+
+        int maxWeight = 0;
+        for(int i=0; i<N; i++){
+            int possible = rope[i] * (N -i);
+            if(possible > maxWeight) maxWeight = possible;
+            
+        }
+
+        System.out.println(maxWeight);
+
+    }
+}
