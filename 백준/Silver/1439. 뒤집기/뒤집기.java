893b83f26c9447eb5cdb7052516f30b6a1357f18
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-11-19 17:39:29 +0900

    [Silver V] Title: 뒤집기, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    static String s;
+    static int zero_cnt = 0, one_cnt = 0;
+    static boolean zero_cont = false, one_cont = false;
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        s = br.readLine();
+
+        for(char c : s.toCharArray()){
+
+            if(c == '0' && !zero_cont ){
+                one_cont = false;
+                zero_cont = true;
+                zero_cnt++;
+            }
+
+            if(c == '1' && !one_cont){
+                zero_cont = false;
+                one_cont = true;
+                one_cnt++;
+            }
+        }
+
+        int result = Math.min(zero_cnt, one_cnt);
+
+        System.out.println(result);
+        
+
+    }
+}
