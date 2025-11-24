522d03129d798b824246f81110a5c4233171415f
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-11-24 13:28:58 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+    static long[] dp;
+    public static void main(String[] args) throws Exception, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp = new long[N+1];
+
+        if(N == 1){
+            System.out.println(1);
+            return;
+        }
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=N; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+        }
+
+        System.out.println(dp[N]);
+
+        
+
+    }
+}
