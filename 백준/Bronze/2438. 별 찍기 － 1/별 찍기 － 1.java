b6226fea850ee8c26a64e9fcc192a7e036622188
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-11-30 23:36:07 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 176 ms, Memory: 14960 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+    public static void main(String[] args) throws Exception, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<N; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
