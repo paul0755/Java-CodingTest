289935b4a88b1169c99fb44cbb5edaaac4ab47a4
Yezandi <108642395+paul0755@users.noreply.github.com>
2025-12-25 21:34:53 +0900

    [Bronze V] Title: N 찍기, Time: 184 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(i).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
